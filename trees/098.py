4eae712106a62f9fa2908c39eeede1727b8cd8e5
Javier Garcia <javierdangarcia@gmail.com>
2024-03-23 00:07:18 -0400

    added 98: validate binary search tree

---
+++ trees/098.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def valid(root, min, max):
+            if not root:
+                return True
+            elif root.val <= min or root.val >= max:
+                return False
+            else:
+                return valid(root.left, min, root.val) and valid(root.right, root.val, max)
+
+        return valid(root, float("-inf"), float("inf"))
+
+# Time: O(n)
+# Space: O(n)
