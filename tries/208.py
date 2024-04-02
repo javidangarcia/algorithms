b51170dad626c5a5a232dd7773dfeef84d081341
Javier Garcia <javierdangarcia@gmail.com>
2024-04-01 20:46:17 -0400

    added 208: implement trie prefix tree

---
+++ tries/208.py
@@ -0,0 +1,36 @@
+class TrieNode():
+    def __init__(self, char = ""):
+        self.char = char
+        self.children = {}
+        self.isWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode(char)
+            curr = curr.children[char]
+        curr.isWord = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return curr.isWord
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for char in prefix:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return True
+
+# Time: O(n)
+# Space: O(n)
